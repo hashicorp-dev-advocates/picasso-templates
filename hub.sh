b52e6e06fac1df1a96637513977a48bdf27bd12d
Alexandra Freeman <alexandra@hashicorp.com>
2022-10-18 08:08:44 -0400

    hub and clean-up

---
+++ hub.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+EDITION="HUB"
+DATE="Virtual community events, delivered in local languages."
+URL="hashi.co/hashitalks-hub"
+
+ID=$(echo $EDITION | tr '[:upper:]' '[:lower:]')
+REGULAR_FONT="fonts/klavika/regular.ttf"
+BOLD_FONT="fonts/klavika/bold.ttf"
+LIGHT_FONT="fonts/klavika/light.ttf"
+COLOR="blue"
+INPUT_DIR="input/hub"
+OUTPUT_DIR="output/fy2023/hub"
+NEXT="Next"
+LOGO_OFFSET="90"
+
+mkdir -p $OUTPUT_DIR
+
+# social
+picasso generate \
+		-t hashitalks/social/twitter_card_hub.hcl \
+		--var edition=$EDITION \
+		--var color=$COLOR \
+		--var date="Virtual community events, delivered in local languages." \
+		--var url=$URL \
+    -o $OUTPUT_DIR/social/twitter_card.png
+
+picasso generate \
+		-t hashitalks/social/email_banner.hcl \
+		--var edition=$EDITION \
+		--var color=$COLOR \
+    --var logo_offset=$LOGO_OFFSET \
+		-o $OUTPUT_DIR/social/email_banner.png
