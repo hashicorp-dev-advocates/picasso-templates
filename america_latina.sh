dbe61c4655e9bb7bcc5a9e4b31f4170720cd5836
Alexandra Freeman <alexandra@hashicorp.com>
2023-03-10 10:40:09 -0500

    sh files for build, deploy and spanish HashiTalks

---
+++ america_latina.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+EDITION="AMÉRICA LATINA"
+DATE="17 January 2024"
+URL="hashi.co/hashitalks-america-latina"
+
+ID=$(echo $EDITION | tr '[:upper:]' '[:lower:]')
+TAGLINE="Hear from and learn with fellow practitioners."
+REGULAR_FONT="fonts/metro/regular.ttf"
+BOLD_FONT="fonts/metro/bold.ttf"
+LIGHT_FONT="fonts/metro/light.ttf"
+COLOR="yellow"
+INPUT_DIR="input/$EDITION"
+OUTPUT_DIR="../hashitalks-assets/2023/AMERICA_LATINA"
+NEXT="Next"
+LOGO_OFFSET="-188"
+TAGLINE_OFFSET="275"
+
+mkdir -p $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR/speakers
+mkdir -p $OUTPUT_DIR/streaming
+mkdir -p $OUTPUT_DIR/social
+
+# social
+picasso generate \
+	-t hashitalks/social/twitter_card.hcl \
+	--var edition="AMÉRICA LATINA" \
+	--var tagline="Hear from and learn with fellow practitioners:" \
+	--var color=$COLOR \
+	--var tagline_offset=$TAGLINE_OFFSET \
+	--var date=$DATE \
+	--var url=$URL \
+    -o $OUTPUT_DIR/social/twitter_card.png
+
+picasso generate \
+	-t hashitalks/social/email_banner.hcl \
+	--var edition="AMÉRICA LATINA" \
+	--var color=$COLOR \
+    --var logo_offset=$LOGO_OFFSET \
+	-o $OUTPUT_DIR/social/email_banner.png
+
+# # background
+#  picasso generate \
+#   -t hashitalks/streamyard/background/default.hcl \
+#   -o $OUTPUT_DIR/streaming/default_background.png \
+#   --var color=$COLOR
+
+# # branding
+# picasso generate \
+#   -t hashitalks/streamyard/overlay/branding.hcl \
+#   -o $OUTPUT_DIR/streaming/branding_overlay.png \
+#   --var edition=$EDITION \
+
+# title
+picasso generate \
+  -t hashitalks/streamyard/overlay/title.hcl \
+  -o $OUTPUT_DIR/streaming/title.png \
+  --var edition=$EDITION \
+  --var color=$COLOR \
+
+# # speakers
+# picasso generate \
+#   -t hashitalks/speakers/speaker.hcl \
+#   -o $OUTPUT_DIR/speakers/ \
+#   --var color=$COLOR \
+#   --var edition=$EDITION \
+#   --var date="December 7-8, 2022" \
+#   --var url=$URL \
+#   --csv $INPUT_DIR/csv/speakers.csv \
+#   --csv-var speaker
+
+# # speakers
+# picasso generate \
+#   -t hashitalks/speakers/speaker_long_name.hcl \
+#   -o $OUTPUT_DIR/speakers/ \
+#   --var color=$COLOR \
+#   --var edition=$EDITION \
+#   --var date="February 17-18" \
+#   --var url=$URL \
+#   --csv $INPUT_DIR/csv/speakers_long_name.csv \
+#   --csv-var speaker
